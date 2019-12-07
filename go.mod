b1b25c6eba18a9c3ed531a10d3c5c55247880994
Qaiyyum Hakimi <qaiyyumhakimi@gmail.com>
2019-12-07 23:29:13 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module notaku.com/main
+
+go 1.12
+
+require (
+	golang.org/x/text v0.3.2 // indirect
+	rsc.io/quote v1.5.2
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "rsc.io/quote"
+)
+
+func main(){
+  fmt.Println("test")
+  return quote.Hello()
+}
