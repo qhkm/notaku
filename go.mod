ec03873578b950d662f531f2d8e08ffcf45bc483
Qaiyyum Hakimi <qaiyyumhakimi@gmail.com>
2019-12-08 12:20:06 +0900

    setup router

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module notaku.com/main
 go 1.12
 
 require (
+	github.com/gin-gonic/gin v1.5.0
 	golang.org/x/text v0.3.2 // indirect
 	rsc.io/quote v1.5.2
 )
--- main.go
+++ main.go
@@ -1,11 +1,52 @@
 package main
 
 import (
-  "fmt"
-  "rsc.io/quote"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"fmt"
 )
 
-func main(){
-  fmt.Println("test")
-  return quote.Hello()
+type Blog struct {
+	id int
+	title string
+	body string
+}
+
+var blogList = []Blog{
+	Blog{
+		id: 1, title: "test1", body: "testbody1",
+	},
+	Blog{
+		id: 2, title: "test2", body: "testbody2",
+	},
+}
+
+// return array of blog
+func getBlog() Blog {
+	firstBlog := Blog{
+		id: 1,
+		title: "testing title",
+		body: "testing body",
+	}
+	return firstBlog
+}
+
+func getBlogList() []Blog {
+	return blogList
+}
+
+func main() {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	blog := getBlog()
+	blogList := getBlogList()
+	fmt.Println(blogList)
+	r.GET("/ping", func (c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"code": http.StatusOK,
+			"message": blog,
+		})
+	})
+	r.Run()
 }
