b5aa7d2dc2051d0ebb45baf53f0823e01eeb6bd3
Qaiyyum Hakimi <qaiyyumhakimi@gmail.com>
2020-01-04 13:36:11 +0800

    remove unnecessary code

--- main.go
+++ main.go
@@ -29,52 +29,6 @@ func checkError(err error) {
 
 }
 
-// func handleGet(c *gin.Context) {
-// 	listOfPost := []Post{
-// 		Post{
-// 			Id:    1,
-// 			Title: "name",
-// 			Body:  "body",
-// 		},
-// 		Post{
-// 			Id:    2,
-// 			Title: "name2",
-// 			Body:  "body2",
-// 		},
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": listOfPost,
-// 	})
-// }
-
-// func handlePost(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": "handlePost",
-// 	})
-// }
-// func handlePut(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": "handlePut",
-// 	})
-// }
-// func handleDelete(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": "handleDelete",
-// 	})
-// }
-
-// func getPong(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": "pong",
-// 	})
-// }
-
-// func getTest(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": "test",
-// 	})
-// }
-
 // not found page
 func NotFound(route *gin.Engine) {
 	route.NoRoute(func(c *gin.Context) {
@@ -89,31 +43,7 @@ func NoMethods(route *gin.Engine) {
 	})
 }
 
-// write code to open file
-
 type QueryError struct {
 	Query string
 	Err   error
 }
-
-// how to declare multiple var in one liner
-// var a int
-// var a,b int
-// var a int, b int
-// var (
-// 	x int
-// 	y int
-// )
-
-// func Login(email, password string) (map[string]interface{}){
-
-// 	account := &Account{}
-// 	err := GetDB().Table("accounts").Where("email = ? ", email).First(account).Error
-// 	if err != nil {
-// 		if err == gorm.ErrRecordNotFound {
-// 			return u.
-// 		}
-// 	}
-// }
-
-// its not very hard. Just declare the var
