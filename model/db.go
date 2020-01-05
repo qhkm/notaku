01b8d836c4441a1a7060ccd01c049a1561d2c927
Qaiyyum Hakimi <qaiyyumhakimi@gmail.com>
2020-01-05 22:48:16 +0800

    remove unnecessary code

--- model/db.go
+++ model/db.go
@@ -30,16 +30,4 @@ func NewDB() (*DB, error) {
 
 	return &DB{db}, nil
 
-	// 	// statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, title TEXT, body TEXT)")
-	// 	// statement.Exec()
-	// 	// statement, _ = database.Prepare("INSERT INTO posts (title, body) VALUES (?, ?)")
-	// 	// statement.Exec("title test", "title body")
-	// 	// rows, _ := database.Query("SELECT * FROM posts")
-	// 	// var id int
-	// 	// var title string
-	// 	// var body string
-	// 	// for rows.Next() {
-	// 	// 	rows.Scan(&id, &title, &body)
-	// 	// 	fmt.Println(strconv.Itoa(id) + ": " + title + " " + body)
-	// 	// }
 }
