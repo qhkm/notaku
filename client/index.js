e0a655dfc447e5c0025b5765728283188297eb7b
QaiyyumHakimi <qaiyyumhakimi@gmail.com>
2020-01-10 11:47:07 +0900

    add fetch function

--- client/index.js
+++ client/index.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import axios from 'axios'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
 const App = () => {
+    const [data, setData] = useState(null);
 
     const fetchData = async () => {
-        const url = 'http://localhost:8080/api/v1/posts'
-        let res = await axios.get(url)
-        console.log(res)
-    }
+        const url = "http://localhost:8080/api/v1/posts";
+        let res = await axios.get(url);
+        setData(res.data.data);
+    };
+    console.log(data);
 
     React.useEffect(() => {
         let data = fetchData();
-    })
+    }, []);
 
     return (
-        <div>test</div>
-    )
-}
+        <div>
+            {data && data.map((el, idx) => <li key={idx}>{el.title}</li>)}
+        </div>
+    );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
