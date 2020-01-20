19bd1928d1d168313707f8c5403f13752e0e6a78
QaiyyumHakimi <qaiyyumhakimi@gmail.com>
2020-01-20 16:36:07 +0900

    add users controller, model

--- controller/user.go
+++ controller/user.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"notaku/model"
@@ -9,19 +11,93 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// GetAllPosts tets
+
+// GetUser test
+func GetUser(c *gin.Context) {
+	pathParam := c.Param("id")
+	id, _ := strconv.Atoi(pathParam)
+
+	userService := model.Env{DB: db.DB}
+	UserList := userService.SingleUser(id)
+	c.JSONP(http.StatusOK, gin.H{
+		"message": "get one user",
+		"data":    UserList,
+		"param":   pathParam,
+	})
+}
+
+// GetAllUsers tets
 func GetAllUsers(c *gin.Context) {
 	userService := model.Env{DB: db.DB}
 	UserList := userService.AllUsers()
-	// post := model.Post{}
-	// err := c.Bind(&post)
-	// if err != nil {
-	// 	c.String(http.StatusBadRequest, "Bad request")
-	// 	return
-	// }
 	c.JSONP(http.StatusOK, gin.H{
 		"message": "ok",
 		"data":    UserList,
 	})
 
+}
+
+
+// UpdateUser test
+func UpdateUser(c *gin.Context) {
+	pathParam := c.Param("id")
+	id, _ := strconv.Atoi(pathParam)
+	objA := model.User{}
+	if errA := c.ShouldBind(&objA); errA != nil {
+		panic(errA.Error())
+	}
+	fmt.Println(objA)
+	objA.ID = id
+	fmt.Println(objA)
+	userService := model.Env{DB: db.DB}
+	ok, err := userService.UpdateUser(objA)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error":  err.Error(),
+			"status": ok,
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success update user",
+	})
+}
+
+// AddUser test
+func AddUser(c *gin.Context) {
+	objA := model.User{}
+	if errA := c.ShouldBind(&objA); errA != nil {
+		panic(errA.Error())
+	}
+	userService := model.Env{DB: db.DB}
+
+	ok, err := userService.AddUser(objA)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error":  err.Error(),
+			"status": ok,
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success add user",
+	})
+}
+
+// DeleteUser test
+func DeleteUser(c *gin.Context) {
+	pathParam := c.Param("id")
+	id, _ := strconv.Atoi(pathParam)
+	userService := model.Env{DB: db.DB}
+	// get id from param
+	// id := c.Params.ByName("id")
+	rowsAffected, err2 := userService.DeleteUser(id)
+	if err2 != nil {
+		panic(err2.Error())
+	}
+	fmt.Println(rowsAffected)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "delete OK",
+	})
 }
--- model/user.go
+++ model/user.go
@@ -1,6 +1,9 @@
 package model
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 type User struct {
 	ID   int `json:"id,omitempty"`
@@ -8,15 +11,33 @@ type User struct {
 	Age  int `json:"age,omitempty"`
 }
 
+// Single user test
+func (env *Env) SingleUser(id int) User {
+	sqlStatement := "SELECT * FROM users WHERE id = ?"
+	results, err := env.DB.Query(sqlStatement, id)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// AllPosts test
+	var user User
+	for results.Next() {
+		err := results.Scan(&user.ID, &user.Name, &user.Age)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println(user)
+	return user
+
+}
+
+
 func (env *Env) AllUsers() []User {
 	results, err := env.DB.Query("SELECT * FROM users")
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// var posts []model.Post
 	var users []User
 	for results.Next() {
 		var user User
@@ -25,9 +46,50 @@ func (env *Env) AllUsers() []User {
 			panic(err.Error())
 		}
 		users = append(users, user)
-		// fmt.Println(strconv.Itoa(post.Id) + ": " + post.Title + " " + post.Body)
 	}
 	fmt.Println(users)
 	return users
 
+}
+
+
+func (env *Env) UpdateUser(user User) (int64, error) {
+	sqlStatement := `UPDATE users SET name = ?, age = ? WHERE id = ?`
+	fmt.Println(user)
+	results, err := env.DB.Exec(sqlStatement, user.Name, user.Age, user.ID)
+	if err != nil {
+		panic(err.Error())
+	}
+	rowsAffected, _ := results.RowsAffected()
+	return rowsAffected, nil
+}
+
+// AddUser test
+func (env *Env) AddUser(user User) (int64, error) {
+	sqlStatement := `INSERT INTO users(name, age) values(?, ?)`
+	results, err := env.DB.Exec(sqlStatement, user.Name, user.Age)
+	if err != nil {
+		panic(err.Error())
+	}
+	id, _ := results.LastInsertId()
+	rowsAffected, _ := results.RowsAffected()
+	fmt.Println(id)
+	return rowsAffected, nil
+}
+
+// DeleteUser test
+func (env *Env) DeleteUser(id int) (int64, error) {
+
+	sqlStatement := `DELETE FROM users where id = ?`
+	results, err := env.DB.Exec(sqlStatement, id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	count, err := results.RowsAffected()
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println(count)
+	return count, nil
 }
